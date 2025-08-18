af4726f29b1008f20838e4b3731be06637795838
KirillP-Honor <202534466+KirPavMos@users.noreply.github.com>
2025-08-18 15:14:43 +0300

    settings.py mod

--- workspace1/settings.py
+++ workspace1/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'workstations.apps.WorkstationsConfig',
+    'employees.apps.EmployeesConfig',
 ]
 
 MIDDLEWARE = [
