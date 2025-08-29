516176538122c412d124761a8927a84c9287bb6f
KirillP-Honor <202534466+KirPavMos@users.noreply.github.com>
2025-08-29 13:30:50 +0300

    final commit

--- workspace1/urls.py
+++ workspace1/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', include('employees.urls')),
 ]
 
+# Добавляем обработку медиафайлов в режиме разработки
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
